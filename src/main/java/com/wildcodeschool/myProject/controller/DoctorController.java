be7c094eed7e564f3d84d9a7a24f63e4449f1fa5
Frutios <benjamin.tourneur7@gmail.com>
2020-07-11 01:10:58 +0200

    quest resolved

---
+++ src/main/java/com/wildcodeschool/myProject/controller/DoctorController.java
@@ -0,0 +1,30 @@
+package com.wildcodeschool.myProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class DoctorController {
+
+    @GetMapping("/doctor/1")
+    @ResponseBody
+    public String first() {
+        return "William Hartnell";
+    }
+
+    @GetMapping("/doctor/10")
+    @ResponseBody
+    public String ten() {
+        return "David Tennant";
+    }
+
+    @GetMapping("/doctor/13")
+    @ResponseBody
+    public String thirteen() {
+        return "Jodie Whittaker";
+    }
+
+
+}
